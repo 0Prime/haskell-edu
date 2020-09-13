83d727c3954cf033af6ccbf4565b7c72aa8083ba
Selyanin Egor <sel-egor@yandex.ru>
2020-09-13 11:26:47 +0000

    rename lenVec to len

--- src/stepic/LenVec.hs
+++ src/stepic/LenVec.hs
@@ -1,10 +1,10 @@
-lenVec :: Floating a => [ a] -> a
-lenVec = sqrt . sum . map (^ 2)
+len :: Floating a => [a] -> a
+len = sqrt . sum . map (^ 2)
 
 lenVec3 :: Floating a => a -> a -> a -> a
-lenVec3 x y z = lenVec [x, y, z]
+lenVec3 x y z = len [x, y, z]
 
-ret =  lenVec3 3 4 5
+ret = lenVec3 3 4 5
 
 main :: IO ()
 main = putStrLn . show $ ret
