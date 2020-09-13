34568b54e0f8029cf6c6a0dab28250f753ec330d
Egor Selyanin <sel-egor@yandex.ru>
2020-09-13 09:40:39 +0000

    add LenVec.hs
    see: https://stepik.org/lesson/7859/step/7

--- src/stepic/LenVec.hs
+++ src/stepic/LenVec.hs
@@ -1,10 +1,10 @@
-lenVec :: Floating a => [a] -> a
+lenVec :: Floating a => [ a] -> a
 lenVec = sqrt . sum . map (^ 2)
 
 lenVec3 :: Floating a => a -> a -> a -> a
 lenVec3 x y z = lenVec [x, y, z]
 
-ret = lenVec3 3 4 5
+ret =  lenVec3 3 4 5
 
 main :: IO ()
 main = putStrLn . show $ ret
