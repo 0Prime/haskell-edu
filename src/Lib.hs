e2d401a0ca8a77999c61b11efb71bdbe9a59c867
Selyanin Egor <sel-egor@yandex.ru>
2020-09-15 04:32:18 +0000

    add digits function

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,7 +1,17 @@
 module Lib
   ( len,
+    digits,
   )
 where
 
 len :: Floating a => [a] -> a
 len = sqrt . sum . map (^ 2)
+
+digits :: Integral a => a -> [a]
+digits x
+  | x == 0 = [0]
+  | x < 0 = digits $ abs x
+  | otherwise = digs x
+  where
+    digs 0 = []
+    digs x = digs (x `div` 10) ++ [x `mod` 10]
