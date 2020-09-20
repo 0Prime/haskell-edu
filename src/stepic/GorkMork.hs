bd145e401b1767d80d8bd1f073b03c52c265decb
Selyanin Egor <sel-egor@yandex.ru>
2020-09-20 07:41:42 +0000

    add GorkMork.hs

---
+++ src/stepic/GorkMork.hs
@@ -0,0 +1,13 @@
+class KnownToGork a where
+  stomp :: a -> a
+  doesEnrageGork :: a -> Bool
+
+class KnownToMork a where
+  stab :: a -> a
+  doesEnrageMork :: a -> Bool
+
+class (KnownToGork a, KnownToMork a) => KnownToGorkAndMork a where
+  stompOrStab :: a -> a
+  stompOrStab a = select doesEnrageMork stomp $ select doesEnrageGork stab a
+    where
+      select p f = if p a then f else id
