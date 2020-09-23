bec171efda1aea58f2fd65f6b09b7e9984d762ff
Selyanin Egor <sel-egor@yandex.ru>
2020-09-23 20:10:48 +0000

    add coins function

--- src/Stepic/Coins.hs
+++ src/Stepic/Coins.hs
@@ -1,13 +1,22 @@
 module Stepic.Coins where
 
-import Data.Functor
 import Data.List (partition)
 
 insert :: [a] -> [a] -> [[a]]
-insert values target = values <&> (\v -> v : target)
+insert values target = map (\v -> v : target) values
 
 shrink :: Int -> [[Int]] -> [[Int]]
 shrink n = filter (\xs -> sum xs <= n)
 
 split :: Int -> [[Int]] -> ([[Int]], [[Int]])
 split n xss = partition (\xs -> sum xs == n) xss
+
+change :: [Int] -> Int -> [[Int]]
+change coins n = generate $ splitter $ nextGen []
+  where
+    generate :: ([[Int]], [[Int]]) -> [[Int]]
+    generate (xss, []) = xss
+    generate (xss, yss) = xss ++ (generate $ splitter $ yss >>= nextGen)
+    nextGen = (shrink n) . inserter
+    inserter = insert coins
+    splitter = split n
