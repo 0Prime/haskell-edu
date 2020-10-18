c4159a785c8d7d39bac2e99537a341393329371c
Selyanin Egor <sel-egor@yandex.ru>
2020-10-18 10:54:39 +0000

    move Point3D and GeomPrimitive to functors

--- src/Stepic/Functors.hs
+++ src/Stepic/Functors.hs
@@ -1,5 +1,16 @@
 module Stepic.Functors where
 
+data Point3D a = Point3D a a a deriving (Show)
+
+instance Functor Point3D where
+  fmap f (Point3D x y z) = Point3D (f x) (f y) (f z)
+
+data GeomPrimitive a = Point' (Point3D a) | LineSegment (Point3D a) (Point3D a)
+
+instance Functor GeomPrimitive where
+  fmap f (Point' a) = Point' (fmap f a)
+  fmap f (LineSegment a b) = LineSegment (fmap f a) (fmap f b)
+
 data Tree a = Leaf (Maybe a) | Branch (Tree a) (Maybe a) (Tree a) deriving (Show)
 
 instance Functor Tree where
