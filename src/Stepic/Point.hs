f5addef304e9e2920a276f9bb47945cdb5a1f3cb
Selyanin Egor <sel-egor@yandex.ru>
2020-10-18 10:37:09 +0000

    add Point3D and GeomPrimitive

--- src/Stepic/Point.hs
+++ src/Stepic/Point.hs
@@ -10,3 +10,14 @@ distanceToOrigin (Point x y) = sqrt (x ^ 2 + y ^ 2)
 
 distance :: Point -> Point -> Double
 distance (Point x1 y1) (Point x2 y2) = distanceToOrigin $ Point (x1 - x2) (y1 - y2)
+
+data Point3D a = Point3D a a a deriving (Show)
+
+instance Functor Point3D where
+  fmap f (Point3D x y z) = Point3D (f x) (f y) (f z)
+
+data GeomPrimitive a = Point' (Point3D a) | LineSegment (Point3D a) (Point3D a)
+
+instance Functor GeomPrimitive where
+  fmap f (Point' a) = Point' (fmap f a)
+  fmap f (LineSegment a b) = LineSegment (fmap f a) (fmap f b)
