45919a8db4e21dbf4b37081ffd1b3efe39215734
Selyanin Egor <sel-egor@yandex.ru>
2020-10-19 19:06:38 +0000

    add Monads module
    upd cabal file

---
+++ src/Stepic/Monads.hs
@@ -0,0 +1,12 @@
+module Stepic.Monads where
+
+data Log a = Log [String] a
+
+toLogger :: (a -> b) -> String -> (a -> Log b)
+toLogger f msg = Log [msg] . f
+
+execLoggers :: a -> (a -> Log b) -> (b -> Log c) -> Log c
+execLoggers a f1 f2 = Log (msg1 ++ msg2) c
+  where
+    Log msg1 b = f1 a
+    Log msg2 c = f2 b
