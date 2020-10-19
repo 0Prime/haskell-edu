5d68b82e601681270cd919e24c7b130a9d2f6783
Selyanin Egor <sel-egor@yandex.ru>
2020-10-19 19:01:47 +0000

    add Log instances for Functor, Applicative and Monad

--- src/Stepic/Monads.hs
+++ src/Stepic/Monads.hs
@@ -16,3 +16,14 @@ returnLog = Log []
 
 bindLog :: Log a -> (a -> Log b) -> Log b
 bindLog (Log msg1 a) f = (\(Log msg2 b) -> Log (msg1 ++ msg2) b) $ f a
+
+instance Functor Log where
+  fmap f (Log msg a) = Log msg $ f a
+
+instance Applicative Log where
+  pure = returnLog
+  (Log msg1 f) <*> (Log msg2 a) = Log (msg1 ++ msg2) $ f a
+
+instance Monad Log where
+  return = returnLog
+  (>>=) = bindLog
