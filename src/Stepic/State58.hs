2b56c7c19f11242d1533a5cfc65e0ecd2e2a4c97
Selyanin Egor <sel-egor@yandex.ru>
2020-10-25 12:20:59 +0000

    housekeeping

--- src/Stepic/State58.hs
+++ src/Stepic/State58.hs
@@ -1,11 +1,11 @@
 module Stepic.State58 where
 
-import Control.Monad.Reader --(Reader)
-import Control.Monad.State --(State)
-import Control.Monad.Writer
+import Control.Monad.Reader (Reader, runReader)
+import Control.Monad.State (MonadState (state), State)
+import Control.Monad.Writer (Writer, runWriter)
 
 readerToState :: Reader r a -> State r a
-readerToState m = state (\r -> (runReader m r, r))
+readerToState m = state $ \r -> (runReader m r, r)
 
 writerToState :: Monoid w => Writer w a -> State w a
 writerToState m =
