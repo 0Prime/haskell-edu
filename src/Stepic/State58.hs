aa5b5a0aa0316cb78bf081872e291e75a9a5a7f5
Selyanin Egor <sel-egor@yandex.ru>
2020-10-25 12:20:10 +0000

    add writerToState function

--- src/Stepic/State58.hs
+++ src/Stepic/State58.hs
@@ -2,6 +2,12 @@ module Stepic.State58 where
 
 import Control.Monad.Reader --(Reader)
 import Control.Monad.State --(State)
+import Control.Monad.Writer
 
 readerToState :: Reader r a -> State r a
 readerToState m = state (\r -> (runReader m r, r))
+
+writerToState :: Monoid w => Writer w a -> State w a
+writerToState m =
+  let (a, w) = runWriter m
+   in state $ \w' -> (a, w <> w')
