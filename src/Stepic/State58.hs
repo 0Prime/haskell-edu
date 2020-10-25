4a305472cae41bcafbf1fd0a6b837881d7623b7c
Selyanin Egor <sel-egor@yandex.ru>
2020-10-25 13:17:28 +0000

    add fibStep and execStateN functions

--- src/Stepic/State58.hs
+++ src/Stepic/State58.hs
@@ -1,7 +1,7 @@
 module Stepic.State58 where
 
 import Control.Monad.Reader (Reader, runReader)
-import Control.Monad.State (MonadState (state), State)
+import Control.Monad.State (MonadState (state), State, execState, modify, replicateM_)
 import Control.Monad.Writer (Writer, runWriter)
 
 readerToState :: Reader r a -> State r a
@@ -11,3 +11,9 @@ writerToState :: Monoid w => Writer w a -> State w a
 writerToState m =
   let (a, w) = runWriter m
    in state $ \w' -> (a, w <> w')
+
+fibStep :: State (Integer, Integer) ()
+fibStep = modify $ \(a, b) -> (b, a + b)
+
+execStateN :: Int -> State s a -> s -> s
+execStateN n = execState . replicateM_ n
