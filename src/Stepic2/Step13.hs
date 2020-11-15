7ccf42387e0212f62358720f8f4acf2f31a51f18
Selyanin Egor <sel-egor@yandex.ru>
2020-11-15 16:28:15 +0000

    add Stepic2.Step13 module

---
+++ src/Stepic2/Step13.hs
@@ -0,0 +1,6 @@
+module Stepic2.Step13 where
+
+import Text.Parsec
+
+getList :: Parsec String u [String]
+getList = many1 digit `sepBy` char ';'
---
+++ test/Stepic2/Step13Spec.hs
@@ -0,0 +1,23 @@
+module Stepic2.Step13Spec (spec) where
+
+import Stepic2.Step13
+import Test.Hspec
+import Test.Hspec.Parsec
+import Text.Parsec
+
+spec :: Spec
+spec = parallel $ do
+  describe "getList" $ do
+    let getListTest = parse getList ""
+
+    it "1 is [1]" $ do
+      getListTest "1" `shouldParse` ["1"]
+
+    it "1;234;56 is [1,234,56]" $ do
+      getListTest "1;234;56" `shouldParse` ["1", "234", "56"]
+
+    it "fails on trailing ';'" $ do
+      getListTest `shouldFailOn` "1;234;56;"
+
+    it "fails on double ';'" $ do
+      getListTest `shouldFailOn` "1;;234;56"
