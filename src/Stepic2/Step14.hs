df7a3b4716af13f99f0acabe07cdd73a3b4fd028
Selyanin Egor <sel-egor@yandex.ru>
2020-11-19 11:30:03 +0000

    add anyChr function

--- src/Stepic2/Step14.hs
+++ src/Stepic2/Step14.hs
@@ -1,1 +1,11 @@
+{-# LANGUAGE DeriveFunctor #-}
+
 module Stepic2.Step14 where
+
+import Data.List
+
+newtype Prs a = Prs {runPrs :: String -> Maybe (a, String)}
+  deriving (Functor)
+
+anyChr :: Prs Char
+anyChr = Prs uncons
--- test/Stepic2/Step14Spec.hs
+++ test/Stepic2/Step14Spec.hs
@@ -1,10 +1,21 @@
 module Stepic2.Step14Spec (spec) where
 
+import Data.Char
 import Stepic2.Step14
 import Test.Hspec
 
 spec :: Spec
 spec = parallel $ do
-  describe "stub description" $ do
-    it "stub test" $ do
-      1 `shouldBe` 1
+  describe "anyChr" $ do
+    it "test 1" $ do
+      runPrs anyChr "A" `shouldBe` Just ('A', "")
+
+    it "test 1" $ do
+      runPrs anyChr "ABC" `shouldBe` Just ('A', "BC")
+
+    it "test 3" $ do
+      runPrs anyChr "" `shouldBe` Nothing
+
+    it "test 4" $ do
+      runPrs (digitToInt <$> anyChr) "BCD"
+        `shouldBe` Just (11, "CD")
