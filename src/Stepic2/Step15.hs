7905a1a7d69cac3d177b4f513c38429113c97528
Selyanin Egor <sel-egor@yandex.ru>
2021-01-17 10:53:40 +0000

    add unCmps3 and unCmps4 functions

--- src/Stepic2/Step15.hs
+++ src/Stepic2/Step15.hs
@@ -1,4 +1,19 @@
+{-# LANGUAGE TypeOperators #-}
+
 module Stepic2.Step15 where
+
+infixr 9 |.|
+
+newtype (|.|) f g a = Cmps {getCmps :: f (g a)} deriving (Eq, Show)
+
+instance (Functor f, Functor g) => Functor (f |.| g) where
+  fmap fun (Cmps x) = Cmps $ (fmap . fmap) fun x
+
+unCmps3 :: Functor f => (f |.| g |.| h) a -> f (g (h a))
+unCmps3 = fmap getCmps . getCmps
+
+unCmps4 :: (Functor f2, Functor f1) => (f2 |.| f1 |.| g |.| h) a -> f2 (f1 (g (h a)))
+unCmps4 = fmap unCmps3 . getCmps
 
 newtype Cmps3 f g h a = Cmps3 {getCmps3 :: f (g (h a))}
   deriving (Eq, Show)
