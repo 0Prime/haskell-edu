208e775d09530ecbb64b378def0c48531289504b
Selyanin Egor <sel-egor@yandex.ru>
2021-01-30 20:26:50 +0000

    add Tree & Preorder & Postorder & Levelorder with Foldable instances

--- src/Stepic2/Step21.hs
+++ src/Stepic2/Step21.hs
@@ -1,3 +1,36 @@
 module Stepic2.Step21 where
 
-x = undefined
+data Tree a = Nil | Branch (Tree a) a (Tree a) deriving (Eq, Show)
+
+newtype Preorder a = PreO (Tree a) deriving (Eq, Show)
+
+newtype Postorder a = PostO (Tree a) deriving (Eq, Show)
+
+newtype Levelorder a = LevelO (Tree a) deriving (Eq, Show)
+
+instance Foldable Tree where
+  foldr f ini (Branch l a r) =
+    g f l $ f a $ g f r ini
+  foldr _ ini _ = ini
+
+instance Foldable Preorder where
+  foldr f ini (PreO (Branch l a r)) =
+    f a $ g f (PreO l) $ g f (PreO r) ini
+  foldr _ ini _ = ini
+
+instance Foldable Postorder where
+  foldr f ini (PostO (Branch l a r)) =
+    g f (PostO l) $ g f (PostO r) $ f a ini
+  foldr _ ini _ = ini
+
+g :: Foldable t => (a -> b -> b) -> t a -> b -> b
+g = flip . foldr
+
+instance Foldable Levelorder where
+  foldr f ini (LevelO tree) =
+    foldr f ini $ flatten [tree]
+
+flatten :: [Tree a] -> [a]
+flatten [] = []
+flatten (Nil : xs) = flatten xs
+flatten ((Branch l a r) : xs) = a : flatten (xs ++ [l, r])
--- test/Stepic2/Step21Spec.hs
+++ test/Stepic2/Step21Spec.hs
@@ -15,3 +15,34 @@ spec = parallel $ do
       it "test 2" $ do
         foldl (++) "!!" (Tr "ab" "cd" "efg")
           `shouldBe` "!!abcdefg"
+
+  describe "Tree" $ do
+    --   3
+    --  / \
+    -- 1   4
+    --  \
+    --   2
+    let testTree =
+          Branch
+            (Branch Nil 1 (Branch Nil 2 Nil))
+            3
+            (Branch Nil 4 Nil)
+
+    let verify f ex =
+          foldr (:) [] (f testTree) `shouldBe` ex
+
+    describe "is Foldable" $ do
+      it "test 1" $ do
+        verify id [1, 2, 3, 4]
+
+      describe "Preorder" $ do
+        it "test 1" $ do
+          verify PreO [3, 1, 2, 4]
+
+      describe "Postorder" $ do
+        it "test 1" $ do
+          verify PostO [2, 1, 4, 3]
+
+      describe "Levelorder" $ do
+        it "test 1" $ do
+          verify LevelO [3, 1, 4, 2]
