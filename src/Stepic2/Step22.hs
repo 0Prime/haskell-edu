a47d31aa93178bb11ad7d6849e38ce4751921403
Selyanin Egor <sel-egor@yandex.ru>
2021-02-19 18:23:00 +0000

    add Traversable instance to Tripple

--- src/Stepic2/Step22.hs
+++ src/Stepic2/Step22.hs
@@ -1,6 +1,7 @@
 module Stepic2.Step22 where
 
 import Control.Applicative ((<*>))
+import Stepic2.Triple11
 
 sequenceA2list :: (Foldable t, Applicative f) => t (f a) -> f [a]
 sequenceA2list = foldr fn (pure [])
@@ -12,3 +13,6 @@ traverse2list :: (Foldable t, Applicative f) => (a -> f b) -> t a -> f [b]
 traverse2list f = foldr (\x y -> (:) <$> f x <*> y) (pure [])
   where
     fn x y = (:) <$> f x <*> y
+
+instance Traversable Triple where
+  traverse f (Tr x y z) = Tr <$> f x <*> f y <*> f z
--- test/Stepic2/Step22Spec.hs
+++ test/Stepic2/Step22Spec.hs
@@ -2,6 +2,7 @@ module Stepic2.Step22Spec (spec) where
 
 import Stepic2.Step21
 import Stepic2.Step22
+import Stepic2.Triple11
 import Test.Hspec
 
 spec :: Spec
@@ -32,3 +33,20 @@ spec = parallel $ do
                      [21, 22, 13],
                      [21, 22, 23]
                    ]
+
+  describe "Triple as Traversable" $ do
+    it "test 1" $ do
+      foldl (++) "!!" (Tr "ab" "cd" "efg")
+        `shouldBe` "!!abcdefg"
+
+    it "test 2" $ do
+      traverse (\x -> if x > 10 then Right x else Left x) (Tr 12 14 16)
+        `shouldBe` Right (Tr 12 14 16)
+
+    it "test 3" $ do
+      traverse (\x -> if x > 10 then Right x else Left x) (Tr 12 8 4)
+        `shouldBe` Left 8
+
+    it "test 4" $ do
+      sequenceA (Tr (Tr 1 2 3) (Tr 4 5 6) (Tr 7 8 9))
+        `shouldBe` Tr (Tr 1 4 7) (Tr 2 5 8) (Tr 3 6 9)
