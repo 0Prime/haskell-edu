8ba3cbf8de73e558b69170ab0c842ff9f3e5b20e
Selyanin Egor <sel-egor@yandex.ru>
2021-03-19 07:19:08 +0000

    add concat3OC

--- src/Stepic2/Step24.hs
+++ src/Stepic2/Step24.hs
@@ -1,8 +1,14 @@
 module Stepic2.Step24 where
 
 import Stepic2.Step14
+import Stepic2.Step23
 
 instance Monad PrsE where
   (>>=) (PrsE f) k = PrsE $ \s -> do
     (a, s') <- f s
     runPrsE (k a) s'
+
+concat3OC :: OddC a -> OddC a -> OddC a -> OddC a
+concat3OC (Un a) (Un b) cs = Bi a b cs
+concat3OC (Un a) (Bi b1 b2 bs) cs = Bi a b1 $ concat3OC (Un b2) bs cs
+concat3OC (Bi a1 a2 as) bs cs = Bi a1 a2 $ concat3OC as bs cs
--- test/Stepic2/Step24Spec.hs
+++ test/Stepic2/Step24Spec.hs
@@ -1,6 +1,7 @@
 module Stepic2.Step24Spec (spec) where
 
 import Stepic2.Step14
+import Stepic2.Step23
 import Stepic2.Step24
 import Test.Hspec
 
@@ -21,3 +22,49 @@ spec = parallel $ do
 
       it "test 3" $ do
         testHelper "BCD" `shouldBe` Left "unexpected B"
+
+  describe "concat3OC" $ do
+    it "test 1" $ do
+      let x = Un 'a'
+      let y = Un 'b'
+      let z = Un 'c'
+      concat3OC x y z `shouldBe` Bi 'a' 'b' (Un 'c')
+
+    it "test 2" $ do
+      let x = Bi 'a' 'b' (Un 'c')
+      let y = Un 'd'
+      let z = Un 'e'
+      concat3OC x y z
+        `shouldBe` Bi 'a' 'b' (Bi 'c' 'd' (Un 'e'))
+
+    it "test 3" $ do
+      let x = Un 'a'
+      let y = Bi 'b' 'c' (Un 'd')
+      let z = Un 'e'
+      concat3OC x y z
+        `shouldBe` Bi 'a' 'b' (Bi 'c' 'd' (Un 'e'))
+
+    it "test 4" $ do
+      let x = Un 'a'
+      let y = Un 'b'
+      let z = Bi 'c' 'd' (Un 'e')
+      concat3OC x y z
+        `shouldBe` Bi 'a' 'b' (Bi 'c' 'd' (Un 'e'))
+
+    it "test 5" $ do
+      let tst1 = Bi 'a' 'b' (Un 'c')
+      let tst2 = Bi 'd' 'e' (Bi 'f' 'g' (Un 'h'))
+      let tst3 = Bi 'i' 'j' (Un 'k')
+      concat3OC tst1 tst2 tst3
+        `shouldBe` Bi
+          'a'
+          'b'
+          ( Bi
+              'c'
+              'd'
+              ( Bi
+                  'e'
+                  'f'
+                  (Bi 'g' 'h' (Bi 'i' 'j' (Un 'k')))
+              )
+          )
