b065d275707d23ca085d0fbb07de79d9327f6f55
Selyanin Egor <sel-egor@yandex.ru>
2021-04-06 17:39:23 +0000

    add Functor and Applicative instance to PrsEP

--- src/Stepic2/Step25.hs
+++ src/Stepic2/Step25.hs
@@ -1,7 +1,5 @@
 module Stepic2.Step25 where
 
-newtype PrsEP a = PrsEP {runPrsEP :: Int -> String -> (Int, Either String (a, String))}
-
 parseEP :: PrsEP a -> String -> Either String (a, String)
 parseEP p = snd . runPrsEP p 0
 
@@ -14,3 +12,35 @@ satisfyEP p = PrsEP $ f . succ
       | otherwise = (n, Left $ err n [h])
 
     err n msg = "pos " ++ show n ++ ": unexpected " ++ msg
+
+charEP :: Char -> PrsEP Char
+charEP c = satisfyEP (== c)
+
+anyEP :: PrsEP Char
+anyEP = satisfyEP (const True)
+
+newtype PrsEP a = PrsEP
+  { runPrsEP ::
+      Int ->
+      String ->
+      (Int, Either String (a, String))
+  }
+
+instance Functor PrsEP where
+  fmap fab (PrsEP fa) = PrsEP fun
+    where
+      fun n s = case fa n s of
+        (n', Right (a, s')) -> (n', Right (fab a, s'))
+        (n', Left e) -> (n', Left e)
+
+instance Applicative PrsEP where
+  pure a = PrsEP $ \n s -> (n, Right (a, s))
+
+  (<*>) (PrsEP fab) (PrsEP fa) = PrsEP fun
+    where
+      fun n s = case fab n s of
+        (n', Right (ab, s')) -> g ab (fa n' s')
+        (n', Left e) -> (n', Left e)
+
+      g f (n'', Right (y, s'')) = (n'', Right (f y, s''))
+      g _ (n'', Left e) = (n'', Left e)
--- test/Stepic2/Step25Spec.hs
+++ test/Stepic2/Step25Spec.hs
@@ -6,16 +6,35 @@ import Test.Hspec
 spec :: Spec
 spec = parallel $ do
   describe "PrsEP" $ do
-    let charEP c = satisfyEP (== c)
+    describe "is Functor and Applicagive" $ do
+      it "test 1" $ do
+        runPrsEP (pure 42) 0 "ABCDEFG"
+          `shouldBe` (0, Right (42, "ABCDEFG"))
+
+      let testP = (,) <$> anyEP <* charEP 'B' <*> anyEP
+
+      it "test 2" $ do
+        runPrsEP testP 0 "ABCDE"
+          `shouldBe` (3, Right (('A', 'C'), "DE"))
+
+      it "test 3" $ do
+        parseEP testP "BCDE"
+          `shouldBe` Left "pos 2: unexpected C"
+
+      it "test 4" $ do
+        parseEP testP ""
+          `shouldBe` Left "pos 1: unexpected end of input"
+
+      it "test 5" $ do
+        parseEP testP "B"
+          `shouldBe` Left "pos 2: unexpected end of input"
 
     it "test 1" $ do
       runPrsEP (charEP 'A') 0 "ABC"
         `shouldBe` (1, Right ('A', "BC"))
-
     it "test 2" $ do
       runPrsEP (charEP 'A') 41 "BCD"
         `shouldBe` (42, Left "pos 42: unexpected B")
-
     it "test 3" $ do
       runPrsEP (charEP 'A') 41 ""
         `shouldBe` (42, Left "pos 42: unexpected end of input")
