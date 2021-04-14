5c7dfec2ebd85a3d39ea4d3d8638f77f61d4deb5
Selyanin Egor <sel-egor@yandex.ru>
2021-04-14 04:31:58 +0000

    add withExcept function

--- src/Stepic2/Step31.hs
+++ src/Stepic2/Step31.hs
@@ -1,8 +1,9 @@
 module Stepic2.Step31 where
 
-newtype Except e a = Except {runExcept :: Either e a} deriving (Show)
-
-except = Except
+newtype Except e a = Except {runExcept :: Either e a}
+  deriving (Eq, Show)
 
 withExcept :: (e -> e') -> Except e a -> Except e' a
-withExcept = undefined
+withExcept f ex = Except $ case runExcept ex of
+  Left e -> Left $ f e
+  Right x -> Right x
--- test/Stepic2/Step31Spec.hs
+++ test/Stepic2/Step31Spec.hs
@@ -8,3 +8,12 @@ spec = parallel $ do
   describe "Except" $ do
     it "smoke test" $ do
       'A' `shouldBe` 'A'
+
+  describe "withExcept" $ do
+    it "applies function when is Left" $ do
+      withExcept succ (Except (Left 'A') :: Except Char ())
+        `shouldBe` Except (Left 'B')
+
+    it "does nothing when is Right" $ do
+      withExcept succ (Except (Right ()) :: Except Char ())
+        `shouldBe` Except (Right ())
