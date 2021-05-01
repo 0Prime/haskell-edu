174e9eff36763f28df68b938f9140f48cab089a4
Selyanin Egor <sel-egor@yandex.ru>
2021-05-01 13:36:07 +0000

    add 'decode' function

--- src/Stepic2/Step32.hs
+++ src/Stepic2/Step32.hs
@@ -1,3 +1,28 @@
 module Stepic2.Step32 where
 
-x = 'a'
+decode c = c []
+
+as ns c = c ns
+
+a ns c = c ns
+
+number :: [Int] -> Int
+number = foldr1 $
+  \l r -> if l > r then l * r else l + r
+
+one = make 1
+
+two = make 2
+
+three = make 3
+
+seventeen = make 17
+
+twenty = make 20
+
+hundred = make 100
+
+thousand = make 1000
+
+make :: Int -> [Int] -> ([Int] -> t) -> t
+make n ns c = c (n : ns)
--- test/Stepic2/Step32Spec.hs
+++ test/Stepic2/Step32Spec.hs
@@ -2,9 +2,36 @@ module Stepic2.Step32Spec (spec) where
 
 import Stepic2.Step32
 import Test.Hspec
+import Test.Hspec.Tables
 
 spec :: Spec
 spec = parallel $ do
+  describe "number function" $ do
+    byExample
+      ("values", "expected")
+      [ ([1], 1),
+        ([2], 2),
+        ([1, 20], 21),
+        ([100, 1], 100),
+        ([3, 20, 100, 1], 123),
+        ([1000, 1], 1000),
+        ([1, 1000, 1], 1001),
+        ([2, 2000], 2002),
+        ([1, 1000, 2], 2001)
+      ]
+      (\xs e -> number xs `shouldBe` e)
+
   describe "decode function" $ do
-    it "test 1" $ do
-      'a' `shouldBe` 'a'
+    byExample
+      ("values", "expected")
+      [ (decode one number, 1),
+        (decode one as a number, 1),
+        (decode one hundred twenty three as a number, 123),
+        (decode one hundred twenty one number, 121),
+        (decode one hundred twenty number, 120),
+        (decode one hundred number, 100),
+        (decode three hundred number, 300),
+        (decode two thousand one number, 2001),
+        (decode two thousand seventeen number, 2017)
+      ]
+      shouldBe
